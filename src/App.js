63a135a9b9439cd8d872bae30f731098f19aed5b
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2023-02-10 11:16:27 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import MyNavBar from "./components/MyNavBar";
+import MyCard from "./components/MyCard";
+import {Carousel} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyNavBar></MyNavBar>
+
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 imgCarrousel"
+                    src="https://images.pexels.com/photos/159213/hall-congress-architecture-building-159213.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Consultoria</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 imgCarrousel"
+                    src="https://images.pexels.com/photos/2889093/pexels-photo-2889093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Capacitaciones</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 imgCarrousel"
+                    src="https://images.pexels.com/photos/2889093/pexels-photo-2889093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Clientes</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+        {/*<MyCard></MyCard>*/}
+
     </div>
   );
 }
---
+++ src/components/MyCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+export default function MyCard(){
+    return(
+        <>
+            <div className="container text-center" style={{marginTop: 50, marginBottom: 50}}>
+                <div className="row align-items-center">
+                    <div className="col">
+                        <div className="card" style={{width: "100%"}}>
+                            <div className="card-body">
+                                <h4 className="card-title">Capacitaciones</h4>
+                                <p>Capacitaciones a medida para su empresa, en grupos pequeños o grandes. Es dar clases y algo mas, con un powerpoint lindo y algo asi</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card" style={{width: "100%"}}>
+                            <div className="card-body">
+                                <h4 className="card-title">Consultoria</h4>
+                                <p>Para toda normativa de lo que sea, cosas asi como la ISO y esas siglas raras que garpan. DGQ y esas de otros paises</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
